1f8926482333b57da43a989e223b068a01ec48b4
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2016-11-03 12:27:32 -0600

    Add ssh config file

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -4,6 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(fill-column 80)
+ '(package-selected-packages
+   (quote
+    (unfill zop-to-char zenburn-theme yari yaml-mode which-key volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line scss-mode ruby-tools revive rainbow-mode rainbow-delimiters ov operate-on-number move-text mmm-mode magit json-mode js2-mode inf-ruby imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company coffee-mode browse-kill-ring beacon anzu ag ace-window)))
  '(safe-local-variable-values (quote ((local-preserve-whitespace . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
