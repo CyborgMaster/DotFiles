1ebec28a4ef8c87f173669d3d1e6739f21fa1f67
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2018-01-11 09:25:34 -0700

    Added optimize image function

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -9,7 +9,7 @@
  '(js2-strict-trailing-comma-warning nil)
  '(package-selected-packages
    (quote
-    (sass-mode markdown-mode narrow-indirect isearch+ groovy-mode less-css-mode unfill zop-to-char zenburn-theme yari yaml-mode which-key volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools revive rainbow-mode rainbow-delimiters ov operate-on-number move-text mmm-mode magit json-mode js2-mode inf-ruby imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company coffee-mode browse-kill-ring beacon anzu ag ace-window)))
+    (textile-mode sass-mode markdown-mode narrow-indirect isearch+ groovy-mode less-css-mode unfill zop-to-char zenburn-theme yari yaml-mode which-key volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools revive rainbow-mode rainbow-delimiters ov operate-on-number move-text mmm-mode magit json-mode js2-mode inf-ruby imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company coffee-mode browse-kill-ring beacon anzu ag ace-window)))
  '(safe-local-variable-values (quote ((local-preserve-whitespace . t))))
  '(tab-width 4))
 (custom-set-faces
--- home/.zshrc
+++ home/.zshrc
@@ -93,3 +93,12 @@ alias agr='alias | grep'
 export LESSOPEN="| pygmentize -f terminal256 %s"
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+optimize-image() {
+    convert -filter Triangle -define filter:support=2 -thumbnail $2 \
+            -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 \
+            -define jpeg:fancy-upsampling=off -define png:compression-filter=5 \
+            -define png:compression-level=9 -define png:compression-strategy=1 \
+            -define png:exclude-chunk=all -interlace none -colorspace sRGB \
+            -strip $1 $3
+}
