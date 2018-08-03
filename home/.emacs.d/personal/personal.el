c50f11e2a0839569c45b70b9a3501bc74d9aac6b
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2018-08-02 22:43:36 -0600

    remove emacs packages that are no longer in melpa

--- home/.emacs.d/personal/personal.el
+++ home/.emacs.d/personal/personal.el
@@ -6,13 +6,6 @@
 (prelude-require-package 'unfill)
 (define-key global-map (kbd "C-M-q") 'unfill-paragraph)
 
-(prelude-require-package 'narrow-indirect)
-(define-key ctl-x-4-map "nd" 'ni-narrow-to-defun-indirect-other-window)
-(define-key ctl-x-4-map "nn" 'ni-narrow-to-region-indirect-other-window)
-(define-key ctl-x-4-map "np" 'ni-narrow-to-page-indirect-other-window)
-
-(prelude-require-package 'isearch+)
-(eval-after-load "isearch" '(require 'isearch+))
 (global-unset-key (kbd "ESC ESC ESC"))
 
 (prelude-require-package 'string-inflection)
