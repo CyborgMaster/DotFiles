7b4fd93756876c0f3e1ef0ee82d113431e9c30a8
Jeremy Mickelson <jeremy.mickelson@gmail.com>
2019-08-09 15:58:45 -0600

    added fill function

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(enh-ruby-bounce-deep-indent nil)
+ '(enh-ruby-preserve-indent-in-heredocs t)
  '(fill-column 80)
  '(isearchp-deactivate-region-flag nil)
  '(js2-bounce-indent-p t)
--- home/.emacs.d/personal/personal.el
+++ home/.emacs.d/personal/personal.el
@@ -3,6 +3,8 @@
             (setq indent-tabs-mode nil
                   c-basic-offset 2)))
 
+(add-hook 'c-mode-hook (lambda () (c-toggle-comment-style -1)))
+
 (prelude-require-package 'unfill)
 (define-key global-map (kbd "C-M-q") 'unfill-paragraph)
 
@@ -14,3 +16,11 @@
 (define-key global-map (kbd "s-b s") 'string-inflection-underscore)
 (define-key global-map (kbd "s-b S") 'string-inflection-upcase)
 (define-key global-map (kbd "s-b k") 'string-inflection-kebab-case)
+
+(defun fill-to-end (char)
+  (interactive "cFill Character:")
+  (save-excursion
+    (end-of-line)
+    (while (< (current-column) 80)
+      (insert-char char))))
+(define-key global-map (kbd "s-b f") 'fill-to-end)
