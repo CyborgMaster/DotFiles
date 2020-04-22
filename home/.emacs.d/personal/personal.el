711a750043766b745908ffe3dfc4422f047cf30a
Jeremy Mickelson <jmickelson@tesla.com>
2020-04-22 15:06:09 -0600

    remove org mode bindings that conflict with windmove

--- home/.emacs.d/personal/personal.el
+++ home/.emacs.d/personal/personal.el
@@ -25,18 +25,29 @@
       (insert-char char))))
 (define-key global-map (kbd "s-b f") 'fill-to-end)
 
-;; Make windmove work in Org mode:
-(add-hook 'org-shiftup-final-hook 'windmove-up)
-(add-hook 'org-shiftleft-final-hook 'windmove-left)
-(add-hook 'org-shiftdown-final-hook 'windmove-down)
-(add-hook 'org-shiftright-final-hook 'windmove-right)
-
-;; In org-mode, swap M-RET and RET
-;;
-;; For most note taking, I want return to insert a new item, not create a new
-;; line, for those rare times I actually want a new line character, I can use
-;; M-RET.
+;; Org mode customizations
+(add-to-list 'auto-mode-alist '("\\.org\\.txt\\'" . org-mode))
 (with-eval-after-load 'org
+  ;; Make windmove work in Org mode:
+  (define-key org-mode-map (kbd "<S-up>") nil)
+  (define-key org-mode-map (kbd "<S-down>") nil)
+  (define-key org-mode-map (kbd "<S-left>") nil)
+  (define-key org-mode-map (kbd "<S-right>") nil)
+
+  ;; This version keeps the basic org mode commands and then moves to another
+  ;; window if the command wouldn't do anything.  This isn't worth it to me.  I
+  ;; would rather have window moving always work, regardless of context.
+  ;;
+  ;; (add-hook 'org-shiftup-final-hook 'windmove-up)
+  ;; (add-hook 'org-shiftleft-final-hook 'windmove-left)
+  ;; (add-hook 'org-shiftdown-final-hook 'windmove-down)
+  ;; (add-hook 'org-shiftright-final-hook 'windmove-right)
+
+  ;; In org-mode, swap M-RET and RET
+  ;;
+  ;; For most note taking, I want return to insert a new item, not create a new
+  ;; line, for those rare times I actually want a new line character, I can use
+  ;; M-RET.
   (let ((old-ret (lookup-key org-mode-map (kbd "RET"))))
     (define-key org-mode-map (kbd "RET") (lookup-key org-mode-map (kbd "M-RET")))
     (define-key org-mode-map (kbd "M-RET") old-ret)))
