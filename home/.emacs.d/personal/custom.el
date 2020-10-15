5c2db154a263e90b52e1dc1a9cabccda58c5b808
Jeremy Mickelson <jmickelson@tesla.com>
2020-10-14 20:28:15 -0700

    dont show which key for C-x, seems to hang with lsp-mode

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -20,7 +20,9 @@
  '(package-selected-packages
    (quote
     (dockerfile-mode go-impl commify helm-rg doom-modeline helm-lsp gotest quelpa-use-package yasnippet use-package dash-at-point protobuf-mode go-mode grip-mode graphql-mode string-inflection csv-mode toml-mode prelude-rust rust-mode textile-mode sass-mode markdown-mode narrow-indirect isearch+ groovy-mode less-css-mode unfill zop-to-char zenburn-theme yari yaml-mode which-key volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools revive rainbow-mode rainbow-delimiters ov operate-on-number move-text mmm-mode magit json-mode js2-mode inf-ruby imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company coffee-mode browse-kill-ring beacon anzu ag ace-window)))
- '(safe-local-variable-values (quote ((local-preserve-whitespace . t)))))
+ '(safe-local-variable-values (quote ((local-preserve-whitespace . t))))
+ '(typescript-indent-level 2)
+ '(which-key-inhibit-regexps (quote ("^C-x$"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
