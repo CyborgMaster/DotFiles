070222d5e8f45bd4c5baa01f66fa15cde1f56ca0
Jeremy Mickelson <jmickelson@tesla.com>
2021-12-16 10:15:30 -0800

    remove red boarders around table of contents in org pdf exports

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -3,10 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-split-window-function 'split-window-horizontally)
  '(elmacro-unwanted-commands-regexps
-   (quote
-    ("^(ido.*)$" "^(smex)$" "file-notify-handle-event" "make-directory" "delete-file")))
+   '("^(ido.*)$" "^(smex)$" "file-notify-handle-event" "make-directory" "delete-file"))
  '(enh-ruby-bounce-deep-indent nil t)
  '(enh-ruby-preserve-indent-in-heredocs t)
  '(fill-column 80)
@@ -17,17 +16,26 @@
  '(js2-bounce-indent-p t)
  '(js2-strict-trailing-comma-warning nil)
  '(org-agenda-restore-windows-after-quit t)
- '(org-agenda-window-setup (quote current-window))
+ '(org-agenda-window-setup 'current-window)
+ '(org-latex-hyperref-template
+   "\\hypersetup{
+ pdfauthor={%a},
+ pdftitle={%t},
+ pdfkeywords={%k},
+ pdfsubject={%d},
+ pdfcreator={%c},
+ pdflang={%L},
+ pdfborder={0 0 0}}
+")
  '(org-startup-indented t)
  '(org-startup-truncated nil)
  '(org-use-speed-commands t)
  '(package-selected-packages
-   (quote
-    (elmacro multi-line dockerfile-mode go-impl commify helm-rg doom-modeline helm-lsp gotest quelpa-use-package yasnippet use-package dash-at-point protobuf-mode go-mode grip-mode graphql-mode string-inflection csv-mode toml-mode prelude-rust rust-mode textile-mode sass-mode markdown-mode narrow-indirect isearch+ groovy-mode less-css-mode unfill zop-to-char zenburn-theme yari yaml-mode which-key volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools revive rainbow-mode rainbow-delimiters ov operate-on-number move-text mmm-mode magit json-mode js2-mode inf-ruby imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company coffee-mode browse-kill-ring beacon anzu ag ace-window)))
- '(safe-local-variable-values (quote ((local-preserve-whitespace . t))))
+   '(elmacro multi-line dockerfile-mode go-impl commify helm-rg doom-modeline helm-lsp gotest quelpa-use-package yasnippet use-package dash-at-point protobuf-mode go-mode grip-mode graphql-mode string-inflection csv-mode toml-mode prelude-rust rust-mode textile-mode sass-mode markdown-mode narrow-indirect isearch+ groovy-mode less-css-mode unfill zop-to-char zenburn-theme yari yaml-mode which-key volatile-highlights vkill undo-tree smartrep smartparens smart-mode-line ruby-tools revive rainbow-mode rainbow-delimiters ov operate-on-number move-text mmm-mode magit json-mode js2-mode inf-ruby imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell enh-ruby-mode elisp-slime-nav easy-kill discover-my-major diminish diff-hl crux company coffee-mode browse-kill-ring beacon anzu ag ace-window))
+ '(safe-local-variable-values '((local-preserve-whitespace . t)))
  '(sh-basic-offset 2)
  '(typescript-indent-level 2)
- '(which-key-inhibit-regexps (quote ("^C-x$"))))
+ '(which-key-inhibit-regexps '("^C-x$")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
